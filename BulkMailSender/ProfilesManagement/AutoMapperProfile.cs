c1dc99f3a173b6902c7307e31ddb2c79abb1fbdf
narrag <narrag@AGSHYD-06>
2021-05-21 11:12:04 +0530

    Latest

--- BulkMailSender/ProfilesManagement/AutoMapperProfile.cs
+++ BulkMailSender/ProfilesManagement/AutoMapperProfile.cs
@@ -18,7 +18,10 @@ namespace BulkMailSender.ProfilesManagement
             CreateMap<TblAllTdsEmail, AllTdsEmailViewModel>().ReverseMap();
             CreateMap<TblAllTdsEmail, EmailUpdateModel>().ReverseMap()
                 .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
-            CreateMap<TblRestructuring, RestructuringViewModel>();
+            CreateMap<TblRestructuring, RestructuringViewModel>()
+                .ForMember(d => d.Confirmation,s=>s.MapFrom(m => m.Confirmation == true ? "YES" : "NO"))
+                .ForMember(d => d.SendCopy, s => s.MapFrom(m => m.SendCopy == true ? "YES" : "NO"))
+                .ForMember(d => d.RespondedDate, s => s.MapFrom(m => m.CreatedOn));
         }
     }
 }
--- BulkMailSender/ViewModels/RestructuringViewModel.cs
+++ BulkMailSender/ViewModels/RestructuringViewModel.cs
@@ -10,8 +10,12 @@ namespace BulkMailSender.ViewModels
         public string Email { get; set; }
         public string VendorCode { get; set; }
         public string VendorName { get; set; }
-        public bool Confirmation { get; set; }
+       // public bool Confirmation { get; set; }
         public string Remarks { get; set; }
-        public bool SendCopy { get; set; }
+       // public bool SendCopy { get; set; }
+
+        public string Confirmation { get; set; }
+        public string SendCopy { get; set; }
+        public DateTime? RespondedDate { get; set; }
     }
 }
